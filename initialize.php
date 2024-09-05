c3139b9420af2dc0d8e9a23985b1a2cb698bfddb
Ananaddev <152951253+Ananaddev@users.noreply.github.com>
2024-09-05 18:13:34 +0530

    Update initialize.php

--- initialize.php
+++ initialize.php
@@ -1,9 +1,8 @@
 <?php
 
-$root = (isset($_SERVER['HTTPS']) ? "https://" : "http://") . $_SERVER['HTTP_HOST'];
-$root .= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+
 $dev_data = array('id'=>'-1','firstname'=>'ANAND','lastname'=>'','username'=>'dev_oretnom','password'=>'1234','last_login'=>'','date_updated'=>'','date_added'=>'');
-if(!defined('base_url')) define('base_url',$root);
+if(!defined('base_url')) define('base_url','http://localhost/social-media/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 // if(!defined('dev_data')) define('dev_data',$dev_data);
 if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
